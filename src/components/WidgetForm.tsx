30eb55b2af2a94a94a6fa119c90a64d55abfdefa
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-05-03 09:31:57 -0300

    Content do Component WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,20 @@
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Welcome
+      <header>
+        <span className="text-xl leading-6">Deixe seu Feedback</span>
+      </header>
+      <p>Welcome</p>
+      <footer className="text-xs  text-neutral-400">
+        Feito com ♥ por{" "}
+        <a
+          className="underline underline-offset-2"
+          target="_blank"
+          href="https://www.linkedin.com/in/marcos-vinicius-lima/"
+        >
+          V7-WEB
+        </a>
+      </footer>
     </div>
   );
 }
