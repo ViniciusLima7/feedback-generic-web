16ac6b9f8fecffefa8eed44c3f5c0aef7416d22f
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-05-03 19:01:11 -0300

    Formtação do Botão Enviar Feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -33,7 +33,21 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full"></div>
+      <form action="" className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:outline-none focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com Detalhes o  que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1  flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-zinc-900 focus:bg-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          500: "#8257e6",
+          300: "#996DFF",
+          500: "#8257E6",
         },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
